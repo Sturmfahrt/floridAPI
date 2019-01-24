95498c7305580738a060d37af69800216ff0db9e
Ethan Marshall <emeraldtails.em@gmail.com>
2019-01-24 07:46:19 -0500

    more work on hotel app

--- Hotel app/src/mainApp.java
+++ Hotel app/src/mainApp.java
@@ -7,27 +7,36 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class mainApp {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		//httpConnection();
+		roomManager();
 	}
 	
 	public static void httpConnection() throws IOException {
-		URL url = new URL("http://example.com");
+		System.out.println("Starting...");
+		URL url = new URL("http://localhost:3000");
+		System.out.println("Connected");
 		HttpURLConnection con = (HttpURLConnection) url.openConnection();
 		con.setRequestMethod("GET");
 		
 		Map<String, String> parameters = new HashMap<>();
 		parameters.put("param1", "val");
 		
+		con.setRequestProperty("Content-Type", "application/json");
+		String contentType = con.getHeaderField("Content-Type");
+		System.out.println(contentType);
+		con.setConnectTimeout(5000);
+		con.setReadTimeout(5000);
 		con.setDoOutput(true);
 		DataOutputStream out = new DataOutputStream(con.getOutputStream());
 		out.writeBytes(ParameterStringBuilder.getParamsString(parameters));
 		out.flush();
 		out.close();
+		
 	}
 	
 	public static class ParameterStringBuilder {
@@ -48,5 +57,70 @@ public class mainApp {
 					: resultString;
 		}
 	}
+	
+	public static void roomManager() {
+		int rooms = 20;
+		int reserved = 0;
+		int inUse = 0;
+		int exit = 0;
+		String s;
+		Scanner sc = new Scanner(System.in);
+		while(exit == 0) {
+			System.out.printf("Available rooms: "+ rooms + "\n" + "Reserved rooms: " + reserved + "\nRooms in use: " + inUse + "\n");
+			System.out.println("Would you like to add a reservation, add a room in use, or remove a room from reservation or use?");
+			System.out.println("ADD RESERVE/ADD USE/REMOVE RESERVE/REMOVE USE/EXIT");
+			s = sc.nextLine();
+			switch(s.toUpperCase()) {
+				case "ADD RESERVE":	
+					if(rooms > 0) {
+						reserved += 1;
+						rooms -= 1;
+					}
+					else {
+						System.out.println("There are no more available rooms!");
+						sc.nextLine();
+					}
+					break;
+				case "ADD USE":
+					if(rooms > 0) {
+						inUse += 1;
+						rooms -= 1;
+					}
+					else {
+						System.out.println("There are no more available rooms!");
+						sc.nextLine();
+					}
+					break;
+				case "REMOVE RESERVE":
+					if(reserved > 0) {
+						reserved -= 1;
+						rooms += 1;
+					}
+					else {
+						System.out.println("There are no rooms reserved!");
+						sc.nextLine();
+					}
+					break;
+				case "REMOVE USE":
+					if(inUse > 0) {
+						inUse -= 1;
+						rooms += 1;
+					}
+					else {
+						System.out.println("There are no rooms in use!");
+						sc.nextLine();
+					}
+					break;
+				case "EXIT":
+					exit = 1;
+					break;
+				default: 
+					System.out.println("Invalid answer");
+					sc.nextLine();
+					break;
+			}
+		}
+		System.out.println("Tschuss!");
 
+	}
 }
