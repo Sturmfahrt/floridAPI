d322c29a162c0fd69a89cb153fe16abf90067af3
CyberHead <sirmaxalot305@gmail.com>
2019-01-28 14:49:04 -0500

    hotel controller has been added. still missing code to delete a room.

---
+++ api/controllers/hotelController.js
@@ -0,0 +1,34 @@
+var mongoose = require('mongoose');
+var hotel = require('../models/hotel');
+
+exports.add_hotel_room = function(req, res) {
+    var new_room = new hotel(req.body);
+    new_room.save(function(err, room) {
+        if(err)
+            res.send(err);
+        res.json(room);
+    });
+};
+
+exports.list_hotel_room = function(req,res) {
+    hotel.find({ $and: [{hotelName: req.query.hotelName}, {roomNumber: req.query.roomNumber}]}, function(err, room) {
+        if(err)
+            res.send(err);
+        res.json(room);
+    });
+};
+
+exports.update_hotel_room = function(req,res) {
+    var docId = req.params.id;
+    hotel.findById(docId, function(err, Hotel) {
+        if (err) return handleError(err);
+        
+        Hotel.set(req.query);
+        Hotel.save(function(err, updatedHotel) {
+            if (err) return handleError(err);
+            res.send(updatedHotel);
+        });
+    });
+};
+
+//put delete hotel room code here.
