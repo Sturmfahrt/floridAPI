0bb3bf58ea4a7a094ef156b66a781883701d2e81
Ethan Marshall <majikrow@gmail.com>
2019-01-30 13:19:08 -0500

    classpath updates

--- Hotel app Java/src/main/java/hotel/hotelAppMaven.java
+++ Hotel app Java/src/main/java/hotel/hotelAppMaven.java
@@ -21,6 +21,18 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.json.JSONObject;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 public class hotelAppMaven {
 
 	public static void main(String[] args) throws IOException {
